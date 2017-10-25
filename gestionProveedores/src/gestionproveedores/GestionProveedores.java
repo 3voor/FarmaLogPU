f5b409d9fe06dae789cc5630b08dc5ee1e206dc6
Pablo Aguero <pablo.aguerom@pucp.pe>
2017-10-25 18:35:04 -0500

    cree el branch de proveedores

---
+++ gestionProveedores/src/gestionproveedores/GestionProveedores.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionproveedores;
+
+/**
+ *
+ * @author Pablo
+ */
+public class GestionProveedores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
