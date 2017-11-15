13fbfa4f5089efa18c83aad8d226069182000389
almendranc1994 <almendra.nunezc@pucp.pe>
2017-11-14 21:53:20 -0500

    Registro Solicitudes de Suministro con Detalle completo y funcionando

--- Vista/AccesoDatos/SolicitudSuministroDA.cs
+++ Vista/AccesoDatos/SolicitudSuministroDA.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Modelo;
 using System.Data.SqlClient;
 using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace AccesoDatos
 {
@@ -24,14 +25,21 @@ namespace AccesoDatos
                 cmd.Connection = conn;
                 cmd.CommandText = "REGISTRAR_SOLICITUD_SUMINISTRO";
                 cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                cmd.Parameters["_idSolicitudSuministro"].Direction = System.Data.ParameterDirection.Output;
-                int id = Convert.ToInt32(cmd.Parameters["_idSolicitudSuministro"]);
-                Console.WriteLine();
-                cmd.Parameters.AddWithValue("_idEmpleado", 1);
-                cmd.Parameters.AddWithValue("_prioridad", s.Prioridad);
-                cmd.Parameters.AddWithValue("_fechaPeticion", s.FechaPeticion);
-                cmd.Parameters.AddWithValue("_institucion", s.Institucion);
+                
+                cmd.Parameters.AddWithValue("@codEmp", 1);
+                cmd.Parameters["@codEmp"].Direction = ParameterDirection.Input;
+                cmd.Parameters.AddWithValue("@prior", s.Prioridad);
+                cmd.Parameters["@prior"].Direction = ParameterDirection.Input;
+                cmd.Parameters.AddWithValue("@fPeticion", s.FechaPeticion);
+                cmd.Parameters["@fPeticion"].Direction = ParameterDirection.Input;
+                cmd.Parameters.AddWithValue("@insti", s.Institucion);
+                cmd.Parameters["@insti"].Direction = ParameterDirection.Input;
+                cmd.Parameters.AddWithValue("@idSolicitudSumnistro", MySqlDbType.Int32);
+                cmd.Parameters["@idSolicitudSumnistro"].Direction = ParameterDirection.Output;
+
                 cmd.ExecuteNonQuery();
+                int id = Convert.ToInt32(cmd.Parameters["@idSolicitudSumnistro"].Value);
+                s.IdSolicitudSuministro = id;
                 conn.Close();
                 return true;
             }
@@ -55,9 +63,12 @@ namespace AccesoDatos
                 cmd.Connection = conn;
                 cmd.CommandText = "INSERTAR_DETALLE_SOLICITUD";
                 cmd.CommandType = System.Data.CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("_idSolSumi", id);
-                cmd.Parameters.AddWithValue("_idInsumo", i.IdInsumo);
-                cmd.Parameters.AddWithValue("_cantidad", i.Cantidad);
+                cmd.Parameters.AddWithValue("@idSolSumi", id);
+                cmd.Parameters["@idSolSumi"].Direction = ParameterDirection.Input;
+                cmd.Parameters.AddWithValue("@idIns", i.IdInsumo);
+                cmd.Parameters["@idIns"].Direction = ParameterDirection.Input;
+                cmd.Parameters.AddWithValue("@cant", i.Cantidad);
+                cmd.Parameters["@cant"].Direction = ParameterDirection.Input;                
                 cmd.ExecuteNonQuery();
                 conn.Close();
                 return true;
